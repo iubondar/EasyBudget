513bd6e21db6a47238f7a5cfd28ca9323944a9d1
Ivan Bondar <ibondar@avito.ru>
2023-01-20 19:12:55 +0300

    Add button to validate and save item

--- EasyBudget/Source/EditItem/EditItemView.swift
+++ EasyBudget/Source/EditItem/EditItemView.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 import Combine
 
+fileprivate struct ErrorInfo: Identifiable {
+    var id: Int
+    let title: String
+    let description: String
+}
+
 struct EditItemView: View {
     @State var date = Date()
     @State var amount = ""
     @State var category: Category?
+    
+    @State private var err: ErrorInfo?
     
     var body: some View {
         Form {
@@ -25,8 +33,58 @@ struct EditItemView: View {
                         self.amount = filtered
                     }
                 }
+            
+            Section {
+                Button {
+                    validateAndSave()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Сохранить")
+                        Spacer()
+                    }
+                }
+            }
         }
         .navigationTitle("Новая запись")
+        .alert(
+            item: $err,
+            content: { error in // 5
+                Alert(
+                    title: Text(error.title),
+                    message: Text(error.description)
+                )
+            }
+        )
+    }
+    
+    /// State management
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    private func validateAndSave() {
+        let errorTitle = "Ошибка значения"
+        
+        guard let category = category else {
+            err = ErrorInfo(id: 1, title: errorTitle, description: "Выбери категорию")
+            return
+        }
+
+        guard !amount.isEmpty else {
+            err = ErrorInfo(id: 2, title: errorTitle, description: "Введи сумму")
+            return
+        }
+        
+        let newItem = Item(context: viewContext)
+        newItem.date = date
+        newItem.category = category
+        newItem.amount = NSDecimalNumber(string: amount)
+
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            err = ErrorInfo(id: 3, title: "Ошибка сохранения", description: nsError.localizedDescription)
+        }
     }
 }
 
